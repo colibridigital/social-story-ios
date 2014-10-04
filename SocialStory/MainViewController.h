50f3f8d2708d9ec40e1e45eeca4c9c3e56d01a08
jamescross91 <james_cross91@hotmail.com>
2014-10-04 18:39:56 +0100

    added draft ui

---
+++ SocialStory/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  SocialStory
+//
+//  Created by Ingrid Funie on 04/10/2014.
+//  Copyright (c) 2014 Colibri Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
---
+++ SocialStory/SuggestingViewController.h
@@ -0,0 +1,13 @@
+//
+//  SuggestingViewController.h
+//  SocialStory
+//
+//  Created by Ingrid Funie on 04/10/2014.
+//  Copyright (c) 2014 Colibri Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuggestingViewController : UIViewController
+
+@end
