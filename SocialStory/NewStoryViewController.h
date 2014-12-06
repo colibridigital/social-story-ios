308dca98b0acb59b4e0eba77aa1301ff933e2bc6
jamescross91 <james_cross91@hotmail.com>
2014-12-06 18:13:03 +0000

    Adding new story controller

---
+++ SocialStory/NewStoryViewController.h
@@ -0,0 +1,13 @@
+//
+//  NewStoryViewController.h
+//  SocialStory
+//
+//  Created by James Cross on 06/12/2014.
+//  Copyright (c) 2014 Colibri Ltd. All rights reserved.
+//
+
+#ifndef SocialStory_NewStoryViewController_h
+#define SocialStory_NewStoryViewController_h
+
+
+#endif
---
+++ SocialStory/NewStoryViewController.m
@@ -0,0 +1,9 @@
+//
+//  NewStoryViewController.m
+//  SocialStory
+//
+//  Created by James Cross on 06/12/2014.
+//  Copyright (c) 2014 Colibri Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
